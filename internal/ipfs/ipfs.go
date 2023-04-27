f4ac373fbd08714c88099fbff359f3c718f7bf1b
akakream <akakream@gmail.com>
2023-04-27 14:31:09 +0200

    use cidv1

--- internal/ipfs/ipfs.go
+++ internal/ipfs/ipfs.go
@@ -10,7 +10,7 @@ import (
 func Add(dirPath string) {
 	// Where your local node is running on localhost:5001
 	sh := shell.NewShell("localhost:5001")
-	cid, err := sh.AddDir(dirPath)
+	cid, err := sh.AddDir(dirPath, shell.CidVersion(1))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s", err)
 		os.Exit(1)
